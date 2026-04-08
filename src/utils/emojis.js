225b55f0d89b6b2a11b8ae5ce10ec34d02b9f018
Jordan <paynejordand+github@gmail.com>
2026-04-08 09:13:23 -0400

    Added yaycat

--- src/utils/emojis.js
+++ src/utils/emojis.js
@@ -2,5 +2,6 @@ module.exports = {
   plusEmojiID: "1395041344971604090",
   minusEmojiID: "1395041317138202694",
   angrycatEmojiID: "1363086419479756931",
+  yaycatEmojiID: "1228550232023765112",
   hakistatcomEmojiID: "1490158364540403752",
 };
